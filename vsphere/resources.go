1ae3160aab2864afcc7a3581f027e0d3d3f4b372
Łukasz Opasiak <kerak19@hotmail.com>
2022-07-22 13:54:07 +0200

    vsphere: removed unused getDistributedPortGroups
    Removed unused view Manager

--- vsphere/resources.go
+++ vsphere/resources.go
@@ -7,7 +7,6 @@ import (
 	"github.com/cycloidio/terracognita/filter"
 	"github.com/cycloidio/terracognita/provider"
 	"github.com/vmware/govmomi/find"
-	"github.com/vmware/govmomi/vim25/mo"
 )
 
 // ResourceType is the type used to define all the Resources
@@ -59,30 +58,6 @@ func getDatastoreClusters(ctx context.Context, vs *vsphere, r *reader, resourceT
 	resources := make([]provider.Resource, 0, len(vms))
 	for _, vm := range vms {
 		r := provider.NewResource(vm.InventoryPath, resourceType, vs)
-		resources = append(resources, r)
-	}
-
-	return resources, nil
-}
-
-func getDistributedPortGroups(ctx context.Context, vs *vsphere, r *reader, resourceType string, filters *filter.Filter) ([]provider.Resource, error) {
-	v, err := r.CreateContainerView(ctx, r.Common.Client().ServiceContent.RootFolder, []string{"DistributedVirtualPortgroup"}, true)
-	if err != nil {
-		return nil, err
-	}
-
-	defer v.Destroy(ctx)
-
-	// Retrieve summary property for all DistributedVirtualPortgroups
-	var dvps []mo.DistributedVirtualPortgroup
-	err = v.Retrieve(ctx, []string{"DistributedVirtualPortgroup"}, []string{}, &dvps)
-	if err != nil {
-		return nil, err
-	}
-
-	resources := make([]provider.Resource, 0)
-	for _, dvp := range dvps {
-		r := provider.NewResource(dvp.ManagedEntity.ExtensibleManagedObject.Self.String(), resourceType, vs)
 		resources = append(resources, r)
 	}
 
